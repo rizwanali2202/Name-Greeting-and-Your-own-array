f75337a39a66b4a4ddd7f5d7a0385ff53d484e47
Rizwan Ali <164035652+rizwanali2202@users.noreply.github.com>
2024-03-29 14:57:14 +0500

    Name, greeting and make your own array
    First make array of friends. which access element once at time.. after that print same greeting message to all friend. also make array of transportation. and make message  “I would like to own a Honda motorcycle.

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+// let names : string[] = ["ali", "fazan", "rizwan", "noman"];
+// names.forEach(friends => console.log(friends));
+// exercise 12 = Greeting
+// let names : string[] = ["ali", "fazan", "rizwan", "noman"];
+// names.forEach(friends => console.log(`"hello i am fine" ${friends}`));
+//exercise 13 = Your Own Array
+let transportationMode = ["bike", "car", "truck", "van"];
+transportationMode.forEach(vehical => console.log(`${vehical}, i like this vehical`));
